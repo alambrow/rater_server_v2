8224788f9a2b70f9f518ffd8fe6512724b7f591e
alambrow <alexander.lambrow@gmail.com>
2021-08-05 09:55:38 -0500

    continuing to migrate

--- rater_api/models/player.py
+++ rater_api/models/player.py
@@ -3,3 +3,4 @@ from django.contrib.auth.models import User
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200)
